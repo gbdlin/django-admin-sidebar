40a2944323292ca384f7a915d4e5988858797713
GwynBleidD <gbd.lin@gmail.com>
2017-05-09 20:34:03 +0200

    Fix

--- settings.py
+++ settings.py
@@ -10,12 +10,9 @@ SU_ADMIN_MENU = [
 class Settings(object):
     """Settings for internal use."""
     def __getattr__(self, name):
-        print('getting setting {}'.format(name))
         if not hasattr(django_settings, name) and name not in globals():
-            print('not found setting {}'.format(name))
             raise ValueError('{} is not found in settings'.format(name))
         s = getattr(django_settings, name, globals()[name])
-        print('found setting {}'.format(name))
         return s
 
 
